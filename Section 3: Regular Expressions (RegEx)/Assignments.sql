4863249cb6bff4bf0f84808f7d53694d4540778c
Rajarshi Basak <rajarshi.basak@colorado.edu>
2022-11-19 14:28:46 +0530

    Update Assignments.sql

--- Section 3: Regular Expressions (RegEx)/Assignments.sql
+++ Section 3: Regular Expressions (RegEx)/Assignments.sql
@@ -1,7 +1,15 @@
 select * from stores where regexp_like(physical_address, '[A-Z][A-Z] \d{5}$');
-
 select * from customers where regexp_like(name, '(S|s)te(ph|v)en');
 
-select name, regexp_instr(name, 'n') from eba_countries  where regexp_instr(name,'n') = 7;	
 
+select name, regexp_instr(name, 'n') from eba_countries  where regexp_instr(name,'n') = 7;	
 select name from eba_countries where regexp_instr(name, '\(.*\)') > 0
+
+
+select name, regexp_substr(name, '^A.+a$') from eba_countries;
+select name, regexp_substr(name, '^\w+ ') from eba_countries;	
+
+
+select email_address, regexp_replace(email_address, 'internalmail$', 'internalmail.com') from customers;
+select email_address, regexp_replace(email_address, '\.', '-') from customers;
+select email_address, regexp_replace(email_address, '(.+)(@)(.+)', '\3\2\1') from customers;
