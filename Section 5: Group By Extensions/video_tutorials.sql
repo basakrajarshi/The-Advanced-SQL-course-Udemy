715a9d9a8ccb5887fd1f28307021e0c2e7c86cf5
Rajarshi Basak <rajarshi.basak@colorado.edu>
2022-12-05 14:26:38 +0530

    Update video_tutorials.sql

--- Section 5: Group By Extensions/video_tutorials.sql
+++ Section 5: Group By Extensions/video_tutorials.sql
@@ -29,3 +29,79 @@ sum(population)
 from eba_countries
 group by nvl(organization_region_id, 0),
 (nvl(region_id, 0), nvl(sub_region_id, 0));
+
+-----------------------------------------------------------------------------
+
+select
+nvl(region_id, 0),
+nvl(sub_region_id, 0),
+sum(population)
+from 
+eba_countries
+group by cube(nvl(region_id, 0), nvl(sub_region_id, 0));
+
+select
+nvl(region_id, 0),
+nvl(sub_region_id, 0),
+grouping_id(nvl(region_id, 0), nvl(sub_region_id, 0)),
+sum(population)
+from 
+eba_countries
+group by rollup(nvl(region_id, 0), nvl(sub_region_id, 0));
+
+select
+nvl(region_id, 0),
+nvl(sub_region_id, 0),
+grouping_id(nvl(region_id, 0), nvl(sub_region_id, 0)),
+sum(population)
+from 
+eba_countries
+group by cube(nvl(region_id, 0), nvl(sub_region_id, 0));
+
+select
+nvl(region_id, 0),
+nvl(sub_region_id, 0),
+nvl(organization_region_id, 0),
+grouping_id(nvl(region_id, 0), nvl(sub_region_id, 0), nvl(organization_region_id, 0)),
+sum(population)
+from 
+eba_countries
+group by rollup(nvl(region_id, 0), nvl(sub_region_id, 0), nvl(organization_region_id, 0));
+
+select
+nvl(region_id, 0),
+nvl(sub_region_id, 0),
+nvl(organization_region_id, 0),
+grouping_id(nvl(region_id, 0), nvl(sub_region_id, 0), nvl(organization_region_id, 0)),
+sum(population)
+from 
+eba_countries
+group by cube(nvl(region_id, 0), nvl(sub_region_id, 0), nvl(organization_region_id, 0));
+
+select
+nvl(region_id, 0),
+nvl(sub_region_id, 0),
+nvl(organization_region_id, 0),
+sum(population)
+from 
+eba_countries
+group by grouping sets(nvl(region_id, 0), nvl(sub_region_id, 0), nvl(organization_region_id, 0), ());
+
+select
+nvl(region_id, 0),
+nvl(sub_region_id, 0),
+nvl(organization_region_id, 0),
+sum(population)
+from 
+eba_countries
+group by grouping sets((nvl(region_id, 0), nvl(sub_region_id, 0)), nvl(organization_region_id, 0), ());
+
+select
+nvl(region_id, 0),
+nvl(sub_region_id, 0),
+nvl(organization_region_id, 0),
+sum(population)
+from 
+eba_countries
+group by nvl(organization_region_id, 0), grouping sets((nvl(region_id, 0), nvl(sub_region_id, 0)), ());
+
